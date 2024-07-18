dbbec02f8f26c39fee44b280076196e6c46bf2a3
LINUSOBURA <linusobura@gmail.com>
2024-07-18 18:02:29 +0300

    Fixed regex

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,8 +7,8 @@ import re
 import sys
 
 pattern = re.compile(
-    r'^\d{1,3}(\.\d{1,3}){3} - \[[^\]]+\] "GET /projects/260 HTTP/1\.1"\
-        \d{3} \d+$')
+    r'^\d{1,3}(\.\d{1,3}){3} - \[[^\]]+\] "GET /projects/260 HTTP/1\.1" \d{3} \d+$'  # nopep8
+)
 
 size = 0
 total_codes = 0
@@ -33,25 +33,24 @@ def print_statistics():
             print(f"{code}: {count}")
 
 
-try:
-    for line in sys.stdin:
-        line = line.strip()
-        if pattern.match(line):
-            parts = line.split(" ")
-            size += int(parts[-1])
-            code = parts[-2]
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            if pattern.match(line):
+                parts = line.split(" ")
+                size += int(parts[-1])
+                code = parts[-2]
 
-            if code in status_code_count:
-                status_code_count[code] += 1
-                total_codes += 1
+                if code in status_code_count:
+                    status_code_count[code] += 1
+                    total_codes += 1
 
-            if total_codes == 10:
-                print_statistics()
+                if total_codes == 10:
+                    print_statistics()
 
-                total_codes = 0
-                status_code_count = {key: 0 for key in status_code_count}
+                    total_codes = 0
+                    status_code_count = {key: 0 for key in status_code_count}
 
-except KeyboardInterrupt:
-    print_statistics()
-
-    sys.exit()
+    finally:
+        print_statistics()
