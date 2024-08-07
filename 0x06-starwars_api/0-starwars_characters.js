cb4b02bd44558ef34343e6d739bef9cb97800968
LINUSOBURA <linusobura@gmail.com>
2024-08-07 23:51:01 +0300

    fixed styling

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,27 @@
 #!/usr/bin/node
 
-const { argv } = require("process");
-const request = require("request");
-const util = require("util");
+const { argv } = require('process');
+const request = require('request');
+const util = require('util');
 const movie = argv[2];
 
-const movie_url = `https://swapi-api.alx-tools.com/api/films/${movie}`;
+const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movie}`;
 
 const requestPromise = util.promisify(request);
 
-async function fetchCharacters() {
+async function fetchCharacters () {
   try {
-    const { body } = await requestPromise(movie_url, { json: true });
+    const { body } = await requestPromise(movieUrl, { json: true });
     const characters = body.characters;
 
-    for (const character_url of characters) {
-      const { body: characterBody } = await requestPromise(character_url, {
-        json: true,
+    for (const characterUrl of characters) {
+      const { body: characterBody } = await requestPromise(characterUrl, {
+        json: true
       });
       console.log(characterBody.name);
     }
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
   }
 }
 
