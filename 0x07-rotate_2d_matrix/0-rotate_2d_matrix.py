41bdeaaac44ca0a90eedd6130011686abdf2b64c
LINUSOBURA <linusobura@gmail.com>
2024-08-15 21:41:36 +0300

    Initial commit

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Rotating 2D Matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """Function to rotate a 2d matrix"""
+    matrix_len = len(matrix)
+
+    for i in range(matrix_len):
+        """transposing the matrix"""
+        for j in range(i, matrix_len):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        """reverse the matrix"""
+        for i in range(matrix_len):
+            matrix[i].reverse()
