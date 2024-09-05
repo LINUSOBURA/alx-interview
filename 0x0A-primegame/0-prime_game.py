6a103d58c0750006307e9882c451e0c3421b2308
LINUSOBURA <linusobura@gmail.com>
2024-09-05 23:50:05 +0300

    fixed styling

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,60 +4,40 @@
 
 def isWinner(x, nums):
     """
-    The prime game is a game played with two players, Maria and Ben.
-    The game is played with a list of numbers, and the players take turns
-    removing prime numbers from the list. The last player who made a move
-    is the winner.
+    Determines the winner of the prime game after x rounds.
+    Returns the name of the player with the most wins or None if tied.
     """
 
     def sieve_of_eratosthenes(n):
         """
         The Sieve of Eratosthenes algorithm to find all prime numbers up to n.
         """
-        primes = []
         sieve = [True] * (n + 1)
         sieve[0] = sieve[1] = False
-        for i in range(2, n + 1):
+        for i in range(2, int(n**0.5) + 1):
             if sieve[i]:
-                primes.append(i)
                 for multiple in range(i * i, n + 1, i):
                     sieve[multiple] = False
+        primes = [i for i in range(n + 1) if sieve[i]]
         return primes
 
-    def play_game(n):
-        """
-        Play the prime game.
-        The game is played with two players, Maria and Ben. The game is played
-        with a list of numbers, and the players take turns removing prime
-        numbers from the list. The last player who made a move is the winner.
-        """
-        primes = sieve_of_eratosthenes(n)
-        remaining_numbers = set(range(1, n + 1))
-        turn = 0  # 0 for Maria, 1 for Ben
+    # Precompute primes up to the maximum n in nums
+    max_n = max(nums)
+    primes = sieve_of_eratosthenes(max_n)
 
-        while primes:
-            prime = primes.pop(0)
-            if prime in remaining_numbers:
-                multiples = {prime * k for k in range(1, n // prime + 1)}
-                remaining_numbers -= multiples
-            if not remaining_numbers.intersection(primes):
-                break
-            turn = 1 - turn
-
-        return 1 - turn  # The winner is the last one who made a move
+    # Precompute the number of primes up to each number in the range
+    prime_count = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if i in primes else 0)
 
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        if n == 1:
-            ben_wins += 1
+        if prime_count[n] % 2 == 1:
+            maria_wins += 1
         else:
-            winner = play_game(n)
-            if winner == 0:
-                maria_wins += 1
-            else:
-                ben_wins += 1
+            ben_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
